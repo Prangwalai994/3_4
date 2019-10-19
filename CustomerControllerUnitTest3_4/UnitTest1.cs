f5b8255b5ac76573fa6424d674dac384f8524dc4
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-19 19:57:10 +0700

    3_4 Modify (1)

---
+++ CustomerControllerUnitTest3_4/UnitTest1.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Testing_the_Return_Type_of_Method3_4.Fundamentals;
+using Assert = NUnit.Framework.Assert;
+
+namespace CustomerControllerUnitTest3_4
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            var controller = new CustomerController();
+            var result = controller.GetCustomer(0);
+
+            // NotFound
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            // NotFound or one of its derivativer
+            Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotOk()
+        {
+
+        }
+
+    }
+}
