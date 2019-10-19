99e0ebdd6514042652f0d42a9e0f877a0617ccb2
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-19 19:48:24 +0700

    Add project files.

---
+++ Testing_the_Return_Type_of_Method3_4/Fundamentals/CustomerController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_the_Return_Type_of_Method3_4.Fundamentals
+{
+    public class CustomerController
+    {
+        public ActionResult GetCustomer(int id)
+        {
+            if (id == 0)
+                return new NotFound();
+
+            return new Ok();
+        }
+    }
+
+    public class ActionResult { }
+
+    public class NotFound : ActionResult { }
+
+    public class Ok : ActionResult { }
+}
---
+++ Testing_the_Return_Type_of_Method3_4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing_the_Return_Type_of_Method3_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
